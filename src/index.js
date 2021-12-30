ff99fbb024da6612d97a70bd61c6d1e97c9cb9d1
walbert <walbertsc@hotmail.com>
2021-12-29 21:45:30 -0300

    dependencia mongoose instalada.

--- src/index.js
+++ src/index.js
@@ -6,9 +6,4 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-    res.send('OK');
-});
-
-
 app.listen(3000, console.log('Server is run'));
