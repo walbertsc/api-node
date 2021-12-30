c977aefc5bc6857d8de16333b484a279a8ca31fd
walbert <walbertsc@hotmail.com>
2021-12-29 23:13:00 -0300

    Alteração na conexão do mongoo

--- src/database/index.js
+++ src/database/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/noderest', { useMongoClient: true });
+//mongoose.connect('mongodb://localhost:27017/noderest', { useMongoClient: true });
+mongoose.connect('mongodb://localhost:27017/noderest');
 mongoose.Promise = global.Promise;
-
 
 module.exports = mongoose;
