f78c1fbf928c0f7da1317074796f2b44140d1945
walbert <walbertsc@hotmail.com>
2022-01-03 21:43:15 -0300

    reorganização controllers

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-require('./app/controllers/authController')(app);
-require('./app/controllers/projectController')(app);
+require('./app/controllers/index')(app);
+
 
 app.listen(3000, console.log('Server is run'));
