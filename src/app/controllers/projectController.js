063ceb37a93dfe74c768abfebc9e5025ff0c9091
walbert <walbertsc@hotmail.com>
2022-01-07 23:52:04 -0300

    Conclusão da api nodejs apos o termino das rotas CRUD

--- src/app/controllers/projectController.js
+++ src/app/controllers/projectController.js
@@ -8,25 +8,93 @@ const router = express.Router();
 
 router.use(authMiddleware);
 
+//Rota List
 router.get('/', async (req, res) => {
-    res.send({ user: req.userId });
+    try {
+       const projects = await Project.find().populate(['user','tasks']);
+       
+       return res.send({ projects });
+    } catch (err) {
+        return res.status(400).send( { error: 'Error loading  projects'}); 
+    }
 });
 
+//Rota Show
 router.get('/:projectId', async (req, res) => {
-    res.send({ user: req.userId });
+    try {
+        const project = await Project.findById(req.params.projectId).populate(['user','tasks']);
+        
+        return res.send({ project });
+     } catch (err) {
+         return res.status(400).send( { error: 'Error loading  project'}); 
+     }
 });
 
+//Rota Create
 router.post('/', async (req, res) => {
-    res.send({ user: req.userId });;
+    
+    try {
+        
+        const { title, description, tasks } = req.body;
+
+        const project = await Project.create({ title, description, user: req.userId });
+
+        await Promise.all(tasks.map(async task => {
+            const projectTask = new Task({ ...task, project: project._id });
+
+            await projectTask.save();
+
+            project.tasks.push(projectTask);
+        }));
+
+        await project.save();
+
+        return res.send({ project });
+    } catch (err) {
+        
+        return res.status(400).send( { error: 'Error creating new project'});
+    }
 });
 
+
+//Rota Put
 router.put('/:projectId', async (req, res) => {
-    res.send({ user: req.userId });
+    try {
+        
+        const { title, description, tasks } = req.body;
+
+        const project = await Project.findByIdAndUpdate(req.params.projectId, { title, description }, {new: true});
+
+        project.tasks = [];
+        await Task.remove({ project: project._id});
+
+        await Promise.all(tasks.map(async task => {
+            const projectTask = new Task({ ...task, project: project._id });
+
+            await projectTask.save();
+
+            project.tasks.push(projectTask);
+        }));
+
+        await project.save();
+
+        return res.send({ project });
+    } catch (err) {
+        
+        return res.status(400).send( { error: 'Error creating new project'});
+    }
 });
 
 
+//Rota Delete
 router.delete('/:projectId', async (req, res) => {
-    res.send({ user: req.userId });
+    try {
+        await Project.findByIdAndRemove(req.params.projectId);
+        
+        return res.send();
+     } catch (err) {
+         return res.status(400).send( { error: 'Error deleting  project'}); 
+     }
 });
 
 
